7132b0043e728cb327d06006e28fc3fec3aad216
Jeremiah McConnell <miah@miah.com>
2022-04-26 06:58:16 -0600

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ use image::*;
 use notify_rust::{Notification, Timeout};
 use std::thread::sleep;
 use std::time::Duration;
+use dxgcap::BGRA8;
 
 fn main() {
     println!("{} started", Local::now());
-    let mut capturer = Capturer::new_with_timeout(0, Duration::from_secs(2)).unwrap();
+    let mut capturer = Capturer::new_with_timeout(0, Duration::from_secs(2)).expect("failed ot open capturer");
 
     let (w, h) = capturer.geometry();
     sleep(Duration::from_millis(100));
 
     let mut decoder = quircs::Quirc::default();
-    let mut last_clip_set = String::with_capacity(5000);
+    let mut last_clip_set = String::with_capacity(10000);
 
     loop {
         // Get a screen shot
@@ -29,7 +30,13 @@ fn main() {
 
         // Convert to image
         let img = ImageBuffer::from_fn(w, h, |x, y| {
-            let px = ps.get(y as usize * w as usize + x as usize).unwrap();
+            let px = match ps.get(y as usize * w as usize + x as usize) {
+                None => {
+                    println!("failed to read pixel from captured image, continuing");
+                    &BGRA8 {b: 0, g: 0, r: 0, a: 0}
+                },
+                Some(p) => p
+            };
             image::Rgb([px.r, px.g, px.b]).to_luma()
         });
 
@@ -39,44 +46,50 @@ fn main() {
         for code in codes {
             let code = match code {
                 Ok(c) => c,
-                Err(_c) => {
+                Err(_) => {
                     println!("{} failed to extract qr code, continuing", Local::now());
                     continue;
                 }
             };
             let decoded = match code.decode() {
                 Ok(d) => d,
-                Err(_d) => {
+                Err(_) => {
                     println!("{} failed to decode qr code, continuing", Local::now());
                     continue;
                 }
             };
-            let decoded_str = match std::str::from_utf8(&decoded.payload) {
+            let decoded = match std::str::from_utf8(&decoded.payload) {
                 Ok(d) => d,
-                Err(_d) => {
+                Err(_) => {
                     println!("{} failed to decode utf8, continuing", Local::now());
                     continue;
                 }
-            }
-            .to_string();
+            };
 
-            if decoded_str.starts_with("AGSF") && decoded_str.ne(&last_clip_set) {
-                match set_clipboard(formats::Unicode, &decoded_str[4..]) {
+            if decoded.starts_with("AGSF") && last_clip_set.ne(decoded) {
+                match set_clipboard(formats::Unicode, &decoded[4..]) {
                     Ok(r) => r,
-                    Err(_r) => {
+                    Err(_) => {
                         println!("{} failed to set clipboard, continuing", Local::now());
                         continue;
                     }
                 }
-                println!("{} set: {}", Local::now(), &decoded_str[4..]);
-                Notification::new()
+
+                // Notify about the update
+                println!("{} set: {}", Local::now(), &decoded[4..]);
+                match Notification::new()
                     .summary("QR Clipped")
-                    .body(&decoded_str[4..])
+                    .body(&decoded[4..])
                     .timeout(Timeout::Milliseconds(2000))
-                    .show()
-                    .unwrap();
-                last_clip_set = decoded_str;
-                break;
+                    .show() {
+                        Ok(r) => r,
+                        Err(_) => {
+                            println!("failed to show notification, continuing");
+                        }
+                    }
+
+                last_clip_set.clear();
+                last_clip_set.push_str(decoded);
             }
         }
 
